b7e4740e3e28fba6ac614918073b707392bdc24b
Deon <deonsummers91@gmail.com>
2018-06-20 15:56:30 -0500

    finished flushes

--- game/src/rankings.rs
+++ game/src/rankings.rs
@@ -1,45 +1,119 @@
 use card::{Card, Suit};
 use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 
 pub enum Ranks {
-  High_Card,
-  One_Pair,
-  Two_Pair,
-  Three_of_a_Kind,
+  HighCard,
+  OnePair,
+  TwoPair,
+  ThreeOfAKind,
   Straight,
   Flush,
-  Full_House,
-  Four_of_a_Kind,
-  Straight_Flush, // Royal Flush Included
+  FullHouse,
+  FourOfAKind,
+  StraightFlush, // Royal Flush Included
 }
 
 pub fn get_rank(card: Vec<Card>) -> Ranks {
-  Ranks::High_Card
+    Ranks::HighCard
 }
 
 // Should return flush
 fn is_flush(cards: Vec<Card>) -> Option<Suit> {
-	let mut suits: HashMap<Suit, u8>  = HashMap::new();
-	for card in cards {
-		let number = suits.entry(card.suit).or_insert(0);
-		*number += 1;
-	};
-	//match {
-	//	true => Some(Suit::Spades),
-	//	_ => None
-	//}
-	// Check to see if is any suits >= 5
-	None
+    let mut suits: HashMap<Suit, u8>  = HashMap::new();
+    for card in cards {
+        let number = suits.entry(card.suit).or_insert(0);
+        *number += 1;
+    }
+    match suits {
+        _ if *suits.get(&Suit::Spades).unwrap_or(&0) >= 5 => Some(Suit::Spades),
+        _ if *suits.get(&Suit::Clubs).unwrap_or(&0) >= 5 => Some(Suit::Clubs),
+        _ if *suits.get(&Suit::Hearts).unwrap_or(&0) >= 5 => Some(Suit::Hearts),
+        _ if *suits.get(&Suit::Diamonds).unwrap_or(&0) >= 5 => Some(Suit::Diamonds),
+        _ => None
+    }
+}
+
+fn is_straight(cards: Vec<Card>) -> Option<Suit> {
+    None
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    
     #[test]
-    fn should_be_a_flush() {
-      let cards = vec![Card{suit:Suit::Spades, rank:14}, Card{suit:Suit::Spades, rank:14}];
+    fn should_be_a_hearts_flush() {
+      let cards = vec![
+          Card{suit:Suit::Hearts, rank:14}, 
+          Card{suit:Suit::Hearts, rank:10}, 
+          Card{suit:Suit::Hearts, rank:2}, 
+          Card{suit:Suit::Hearts, rank:4}, 
+          Card{suit:Suit::Hearts, rank:6}
+      ];
+      assert_eq!(is_flush(cards), Some(Suit::Hearts));
+    }
+
+    #[test]
+    fn should_be_a_diamonds_flush() {
+      let cards = vec![
+          Card{suit:Suit::Diamonds, rank:14}, 
+          Card{suit:Suit::Diamonds, rank:10}, 
+          Card{suit:Suit::Diamonds, rank:2}, 
+          Card{suit:Suit::Diamonds, rank:4}, 
+          Card{suit:Suit::Diamonds, rank:6}
+      ];
+      assert_eq!(is_flush(cards), Some(Suit::Diamonds));
+    }
+
+    #[test]
+    fn should_be_a_clubs_flush() {
+      let cards = vec![
+          Card{suit:Suit::Clubs, rank:14}, 
+          Card{suit:Suit::Clubs, rank:10}, 
+          Card{suit:Suit::Clubs, rank:2}, 
+          Card{suit:Suit::Clubs, rank:4}, 
+          Card{suit:Suit::Clubs, rank:6}
+      ];
+      assert_eq!(is_flush(cards), Some(Suit::Clubs));
+    }
+
+    #[test]
+    fn should_be_a_spades_flush() {
+      let cards = vec![
+          Card{suit:Suit::Spades, rank:14}, 
+          Card{suit:Suit::Spades, rank:10}, 
+          Card{suit:Suit::Spades, rank:2}, 
+          Card{suit:Suit::Spades, rank:4}, 
+          Card{suit:Suit::Spades, rank:6}
+      ];
       assert_eq!(is_flush(cards), Some(Suit::Spades));
+    }
+
+    #[test]
+    fn should_not_be_a_flush_1() {
+      let cards = vec![
+          Card{suit:Suit::Spades, rank:14}, 
+          Card{suit:Suit::Hearts, rank:10}, 
+          Card{suit:Suit::Spades, rank:2}, 
+          Card{suit:Suit::Spades, rank:4}, 
+          Card{suit:Suit::Spades, rank:6}
+      ];
+      assert_eq!(is_flush(cards), None);
+    }
+
+    #[test]
+    fn should_not_be_a_flush_2() {
+      let cards = vec![
+          Card{suit:Suit::Hearts, rank:5}, 
+          Card{suit:Suit::Hearts, rank:2}, 
+          Card{suit:Suit::Clubs, rank:5}, 
+          Card{suit:Suit::Clubs, rank:2}, 
+          Card{suit:Suit::Diamonds, rank:5}
+      ];
+      assert_eq!(is_flush(cards), None);
     }
 }
