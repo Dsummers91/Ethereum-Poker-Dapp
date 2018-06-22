d05bd2d482ceabc1ac79c54c1df96e0455c8af2d
Deon <deonsummers91@gmail.com>
2018-06-22 14:58:31 -0500

    use hands instead of cards

--- game/src/player.rs
+++ game/src/player.rs
@@ -1,16 +1,15 @@
 use card::{Card};
-//use deck::{Deck};
 use table::{Table};
-//use hand::{Hand};
+use hand::{Hand};
 
 pub struct Player {
-  pub hand: Vec<Card>,
+  pub hand: Hand,
   pub chips: u64,
 }
 
 impl Player {
   fn sit(_table: &mut Table, _seat: u8) {
-
+    
   }
 }
 
