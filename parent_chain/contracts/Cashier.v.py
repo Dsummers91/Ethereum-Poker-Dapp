a4fdbdef8efcfd2b6b4a07fc43c2bb0bd1fa85ce
Deon <deonsummers91@gmail.com>
2018-07-16 16:48:33 -0500

    add verify method

--- parent_chain/contracts/Cashier.v.py
+++ parent_chain/contracts/Cashier.v.py
@@ -1,5 +1,6 @@
 owner: public(address)
 balances: uint256(wei)[address]
+verified_roots: bytes32[bool]
 
 @public
 def __init__():
@@ -19,4 +20,19 @@ def balanceOf(user: address) -> uint256(wei):
 @payable
 def __default__():
   self.balances[msg.sender] = self.balances[msg.sender] + msg.value  
+
+
+## Vyper port of https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/MerkleProof.sol
+@constant
+@public
+def verify(proofs: bytes32[100], root: bytes32, leaf: bytes32) -> bool:
+    computed_hash: bytes32 = leaf
+    for proof in proofs:
+        if leaf < proof:
+            computed_hash = sha3(concat(computed_hash, proof))
+        else:
+            computed_hash = sha3(concat(proof, computed_hash))
+
+    return computed_hash == root
+
 
