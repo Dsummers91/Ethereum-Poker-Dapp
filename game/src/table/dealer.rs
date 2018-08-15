d11ec7bfd4b2a3070e20584dc35bb671ee257d6a
Deon <deonsummers91@gmail.com>
2018-08-14 21:46:01 -0500

    add a little things

--- game/src/table/dealer.rs
+++ game/src/table/dealer.rs
@@ -3,11 +3,23 @@ use table::{Table};
 
 pub trait Deal {
     fn deal(&mut self);
+    fn show_flop(&mut self);
 }
 
 
 impl<'a, 'b, 'c, 'd> Deal for Table<'a, 'b, 'c, 'd> {
     fn deal(&mut self) {
+        for player in self.players.iter() {
+            let card = self.deck.remove(1);
+            self.board.push(card);
+            println!("{:?}",  player);
+        }
+        let game_type = &self.game;
+        self.round = self.round + 1;
+        println!("{:?}", game_type);
+    }
+
+    fn show_flop(&mut self) {
         println!("{:?}", self.deck);
         let card = self.deck.remove(1);
         self.board.push(card);
--- game/src/table/test.rs
+++ game/src/table/test.rs
@@ -22,4 +22,14 @@ mod tests {
         table.deal();
 		assert_eq!(table.game, GameTypes::TexasHoldem);
 	}
+	fn should_be_able_to_deal_to_players() {
+        let mut deck = Deck::new();
+        let player: Option<&mut Player> = None;
+        let player_2: Option<&mut Player> = None;
+        const SEATS: usize = 2;
+        let mut players: [Option<&mut Player>; SEATS] = [player, player_2];
+		let mut table = Table::new(GameTypes::TexasHoldem, &mut deck, &mut players, SEATS);
+        table.deal();
+		assert_eq!(table.game, GameTypes::TexasHoldem);
+	}
 }
